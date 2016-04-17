a955134d0688b3884ea578ee79b51cf87738f97c
Jose Genaro <jose_28gena@live.com>
2016-04-16 21:54:48 -0700

    EMPRESA FORM

---
+++ Cms/Areas/Admin/Controllers/EmpresaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model.Entities;
+
+namespace Cms.Areas.Admin.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        private CmsContext db = new CmsContext();
+
+        // GET: Admin/Empresa
+        public ActionResult Index()
+        {
+            return View(db.Empresa.ToList());
+        }
+
+        // GET: Admin/Empresa/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // GET: Admin/Empresa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Empresa/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idEmpresa,Nombre,Imagen,Vision,Mision,NumeroCalle,Calle,Colonia,Pais,Coordenadas,Telefono,Celular,Correo,Facebook,Twitter,CreadoFecha,CreadoPor,ActualizadoPor,ActualizadoFecha")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empresa.Add(empresa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empresa);
+        }
+
+        // GET: Admin/Empresa/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: Admin/Empresa/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idEmpresa,Nombre,Imagen,Vision,Mision,NumeroCalle,Calle,Colonia,Pais,Coordenadas,Telefono,Celular,Correo,Facebook,Twitter,CreadoFecha,CreadoPor,ActualizadoPor,ActualizadoFecha")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empresa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empresa);
+        }
+
+        // GET: Admin/Empresa/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: Admin/Empresa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empresa empresa = db.Empresa.Find(id);
+            db.Empresa.Remove(empresa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Model/Entities/Empresa.cs
+++ Model/Entities/Empresa.cs
@@ -1,5 +1,6 @@
-namespace Model.Entities
+﻿namespace Model.Entities
 {
+    using Repository.Interfaces;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
@@ -7,7 +8,7 @@ namespace Model.Entities
     using System.Data.Entity.Spatial;
 
     [Table("Empresa")]
-    public partial class Empresa
+    public partial class Empresa:IAuditable
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Empresa()
@@ -31,7 +32,7 @@ namespace Model.Entities
 
         [StringLength(100)]
         public string Mision { get; set; }
-
+        [Display(Name = "#Domicilio")]
         [StringLength(10)]
         public string NumeroCalle { get; set; }
 
@@ -47,25 +48,33 @@ namespace Model.Entities
         [StringLength(100)]
         public string Coordenadas { get; set; }
 
-        [StringLength(50)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Teléfono")]
+        [Required(ErrorMessage = "Número telefonico requerido.")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Ingrese un número telefonico valido.")]
         public string Telefono { get; set; }
-
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Celular")]
+       
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Ingrese un número celular valido.")]
         [StringLength(50)]
         public string Celular { get; set; }
-
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "El e-mail es un campo requerido.")]
+        [EmailAddress(ErrorMessage = "EL e-mail es invalido.")]
         [StringLength(100)]
         public string Correo { get; set; }
 
         [StringLength(100)]
         public string Facebook { get; set; }
-
+        [Url]
         [StringLength(100)]
         public string Twitter { get; set; }
 
         public DateTime CreadoFecha { get; set; }
 
         public int CreadoPor { get; set; }
-
+        
         public int ActualizadoPor { get; set; }
 
         public DateTime ActualizadoFecha { get; set; }
